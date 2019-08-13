6a5a500327ef8a76560a94e0f57e4a0b7eed73ca
soheilysf <soheil.ysf@gmail.com>
2019-08-13 14:15:19 -0700

    fixed html encoding error

--- posenet/converter/wget.py
+++ posenet/converter/wget.py
@@ -1,6 +1,6 @@
-import urllib.request
-import posixpath
+import requests
 import json
+import posixpath
 import os
 
 from posenet import MOBILENET_V1_CHECKPOINTS
@@ -8,17 +8,29 @@ from posenet import MOBILENET_V1_CHECKPOINTS
 GOOGLE_CLOUD_STORAGE_DIR = 'https://storage.googleapis.com/tfjs-models/weights/posenet/'
 
 
-def download_file(checkpoint, filename, base_dir):
-    url = posixpath.join(GOOGLE_CLOUD_STORAGE_DIR, checkpoint, filename)
-    urllib.request.urlretrieve(url, os.path.join(base_dir, checkpoint, filename))
+def download_json(checkpoint, filename, base_dir):
 
+    url = posixpath.join(GOOGLE_CLOUD_STORAGE_DIR, checkpoint, filename)
+    response = requests.get(url)
+    data = json.loads(response.content)
+
+    with open(os.path.join(base_dir, checkpoint, filename), 'w') as outfile:
+        json.dump(data, outfile)
+
+def download_file(checkpoint, filename, base_dir):
+
+    url = posixpath.join(GOOGLE_CLOUD_STORAGE_DIR, checkpoint, filename)
+    response = requests.get(url)
+    f = open(os.path.join(base_dir, checkpoint, filename), 'wb')
+    f.write(response.content)
+    f.close()
 
 def download(checkpoint, base_dir='./weights/'):
     save_dir = os.path.join(base_dir, checkpoint)
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
-    download_file(checkpoint, 'manifest.json', base_dir)
+    download_json(checkpoint, 'manifest.json', base_dir)
 
     f = open(os.path.join(save_dir, 'manifest.json'), 'r')
     json_dict = json.load(f)
